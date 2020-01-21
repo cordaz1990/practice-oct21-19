7575f444dfe77d821122feff317f8063afa5ad43
Cynthia Ordaz <cordaz@dons.usfca.edu>
2020-01-20 18:16:58 -0800

    Update practice.py

--- practice.py
+++ practice.py
@@ -1,4 +1,23 @@
-print("hello")
-print("world")
-print("hello")
-print("world")
+rock = 'anthracite'
+rock[9]
+'e'
+rock[0:3]
+'ant'
+rock[-5:]
+'ancite'
+for character in rock[:5]:
+    print(character)
+    
+a
+n
+t
+h
+r
+
+bases = ('A', 'C', 'G','T')
+for base in bases:
+    print(base)
+A
+C
+G
+T
